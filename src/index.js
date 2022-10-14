04d316b6530dd58bca9270032bdde025e2d2e1db
Ivan Mercedes <ivanmercede@gmail.com>
2022-10-14 10:52:24 -0400

    refactor: 🔥 make this code more better 🔥

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 require("colors");
 const fs = require("fs");
-const path = require("path");
 const inquirer = require("inquirer");
 const execSync = require("child_process").execSync;
 
@@ -11,24 +10,28 @@ frameworks_list.Laravel = {
   packages: "npm install -D tailwindcss postcss autoprefixer",
   desc: "(Vite only)",
   path_css: "/resources/css/app.css",
+  content: `content: ["./resources/**/*.blade.php", "./resources/**/*.js", "./resources/**/*.vue",]`,
 };
 
 frameworks_list["Next.js"] = {
   packages: frameworks_list.Laravel.packages,
   desc: "",
   path_css: "/styles/globals.css",
+  content: `content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}",]`,
 };
 
 frameworks_list.Vite = {
   packages: frameworks_list.Laravel.packages,
   desc: "(Ract and Vue)",
   path_css: "/src/index.css",
+  content: `content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}",]`,
 };
 
 frameworks_list.CRA = {
   packages: frameworks_list.Laravel.packages,
   desc: "(Create React App)",
   path_css: "/src/index.css",
+  content: `content: ["./src/**/*.{js,jsx,ts,tsx}",]`,
 };
 
 frameworks_list.exit = {};
@@ -51,7 +54,7 @@ const inquirerMenu = async () => {
   return option;
 };
 
-const main = async () => {
+const init = async () => {
   let opt;
   do {
     opt = await inquirerMenu();
@@ -60,56 +63,66 @@ const main = async () => {
       case "Vite":
       case "Laravel":
       case "CRA":
-        installAllPackages(frameworks_list[opt].packages, opt);
-        addDirectiveCSS(frameworks_list[opt].path_css);
+        tailwind_init(frameworks_list[opt]);
         opt = "exit";
         break;
     }
   } while (opt !== "exit"); // Stop  the CLI
 };
 
-function installAllPackages(command, fileName) {
+/**
+ * Install tailwindcss and its peer
+ * dependencies via npm, and then run the init command
+ * to generate both tailwind.config.js and postcss.config.js.
+ */
+function tailwind_init(framework) {
   try {
     console.log("Installing tailwindcs...");
-    execSync(command, {
+    execSync(framework.packages, {
       stdio: "inherit",
     });
-    copyConfigToDirectory(fileName);
+    execSync("npx tailwindcss init -p", {
+      stdio: "inherit",
+    });
+    setup_paths(framework.content);
+    tailwind_directives(framework.path_css);
   } catch (error) {
     console.log(error.red);
   }
 }
 
-function copyConfigToDirectory(fileName) {
-  fs.copyFile(
-    path.join(__dirname, `/stubs/${fileName.toLowerCase()}.config.stub`),
-    process.cwd() + "/tailwind.config.js",
-    (err) => {
-      if (err) throw err;
-      console.log(
-        "Created Tailwind CSS config file: " + "tailwind.config.js".green,
-      );
-    },
-  );
-
-  fs.copyFile(
-    path.join(__dirname, `/stubs/postcss.config.stub`),
-    process.cwd() + "/postcss.config.js",
-    (err) => {
-      if (err) throw err;
-      console.log("Created PostCSS config file: " + "postcss.config.js".green);
-    },
-  );
-}
-
-function addDirectiveCSS(path) {
+// Add the @tailwind directives for each of Tailwind’s layers
+function tailwind_directives(path) {
   const DIRECTIVES =
     "@tailwind base;\n@tailwind components;\n@tailwind utilities;";
 
   fs.appendFile(process.cwd() + path, DIRECTIVES, function (err) {
     if (err) throw err;
-    console.log("Tailwind directives added to: " + path.green);
+    console.log("Tailwindcss directives added to: " + path.green);
   });
 }
 
-main();
+// Add the paths to all of your template files in your tailwind config
+function setup_paths(content) {
+  const tailwind_filename = fs.existsSync("tailwind.config.js")
+    ? "/tailwind.config.js"
+    : "/tailwind.config.cjs";
+
+  fs.readFile(process.cwd() + tailwind_filename, "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    const new_content = data.replace("content: []", content);
+
+    fs.writeFile(
+      process.cwd() + tailwind_filename,
+      new_content,
+      "utf8",
+      function (err) {
+        if (err) console.log(err);
+      },
+    );
+  });
+}
+
+init();
